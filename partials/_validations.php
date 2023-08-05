5ddc9a914e3aa97fdcc33148435064f8e63f3905
RootViper4 <fracnkysourya.com>
2023-08-06 01:06:24 +0300

    image updload

--- partials/_validations.php
+++ partials/_validations.php
@@ -1,9 +1,27 @@
 <?php
 
 session_start();
+require_once("db.php");
 if (empty($_SESSION['id_user'])) {
     header("location: index");
     exit();
 } else {
     $id_user = $_SESSION['id_user'];
+}
+
+
+$name = $email = $lastname = $profileimage = $birthday = $degree = $phone = $countryname = $placeofbirth = $gender = "";
+
+$sql = "SELECT * FROM users WHERE id_user= $id_user";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $name = $row['name'];
+        $email = $row['email'];
+        $lastname = $row['last_name'];
+        $birthday =  $row['birthday'];
+        $profileimage = $row['profile_image'];
+        $gender =  $row['gender'];
+    }
 }
